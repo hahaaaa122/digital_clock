f9db670d0ef59252a9b0c01a9cbf1c60def2d795
boiboi <hatuanhungxhvp@gmail.com>
2022-08-11 10:44:06 +0700

    upload source code

---
+++ main.js
@@ -0,0 +1,26 @@
+const hourEl = document.getElementById("hour");
+const minuteEL = document.getElementById("minutes");
+const secondEL = document.getElementById("secounds");
+const ampmEL = document.getElementById("ampm");
+
+function clock() {
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let ampm = "AM"
+    if (h > 12) {
+        h = h - 12
+        ampm = "PM"
+    }
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+    hourEl.innerText = h;
+    minuteEL.innerText = m;
+    secondEL.innerText = s;
+    ampmEL.innerText = ampm;
+    setTimeout(() => {
+        clock()
+    }, 1000)
+}
+clock();
